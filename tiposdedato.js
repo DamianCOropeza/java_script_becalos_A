46e5c14309c38d24c6529e02844b89032622a070
Damian <zacoropezaomg@gmail.com>
2025-02-25 14:34:53 -0600

    hasta la clase 1

---
+++ tiposdedato.js
@@ -0,0 +1,23 @@
+// Este es un comentario uwu
+
+//Console.log es una fx q se utiliza para imprimir dentro de la consola
+console.log("Hola mundo");
+
+//Declaración de variables
+var nombre = "Juanito";
+var edad = 33;
+var sueldo = 1.8888;
+var casada = false;
+var estudio = true;
+var hijos = null; //ausensia de valor
+var dato;
+
+console.log(typeof(nombre));
+console.log(typeof(edad));
+console.log(typeof(sueldo));
+console.log(typeof(casada));
+console.log(typeof(estudio));
+console.log(typeof(hijos));
+console.log(typeof(dato));
+
+var nombre = "Amelie";
